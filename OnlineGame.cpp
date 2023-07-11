a4388eb4de2f01ed874602956f2d554ed24e46aa
RuriMashiro <1445089502@qq.com>
2023-07-11 12:35:05 +0800

    位置限制正确

--- OnlineGame.cpp
+++ OnlineGame.cpp
@@ -5,32 +5,32 @@ const int type = 5; // 圆圈个数
 int nowcircle = 0;  // 当前圆圈数
 // 核心思路：二进制数，每个数字都是不重复的。顺时针八个方向对应2进制，比如：正北，1000 0000 = 128
 
-// 不限制位置，十进制转二进制
-void decimalToBinary(int decimal, int binaryArray[][8])
-{
-    for (int i = 7; i >= 0; i--)
-    {
-        binaryArray[nowcircle][i] = decimal % 2;
-        decimal /= 2;
-    }
-}
-// 限定位置生成十进制数字 试图优化
-int generateRandomDecimal(int binaryArray[][8])
-{
-    int minDecimal = 0;
-    int maxDecimal = 255;
+// // 不限制位置，十进制转二进制
+// void decimalToBinary(int decimal, int binaryArray[][8])
+// {
+//     for (int i = 7; i >= 0; i--)
+//     {
+//         binaryArray[nowcircle][i] = decimal % 2;
+//         decimal /= 2;
+//     }
+// }
+// // 限定位置生成十进制数字 试图优化
+// int generateRandomDecimal(int binaryArray[][8])
+// {
+//     int minDecimal = 0;
+//     int maxDecimal = 255;
 
-    for (int i = 0; i < 8; i++)
-    {
-        if (binaryArray[nowcircle][i] == 1)
-        {
-            minDecimal += (1 << (7 - i));
-        }
-    }
+//     for (int i = 0; i < 8; i++)
+//     {
+//         if (binaryArray[nowcircle][i] == 1)
+//         {
+//             minDecimal += (1 << (7 - i));
+//         }
+//     }
 
-    int randomDecimal = minDecimal + rand() % (maxDecimal - minDecimal + 1);
-    return randomDecimal;
-}
+//     int randomDecimal = minDecimal + rand() % (maxDecimal - minDecimal + 1);
+//     return randomDecimal;
+// }
 // // 二进制转十进制
 // int binaryToDecimal(int binaryArray[][8])
 // {
@@ -69,16 +69,16 @@ int main()
         circleX[i] = -1;
         circleY[i] = -1;
     }
-    // 二进制位
-    int binaryArray[n][8];
+    // // 二进制位
+    // int binaryArray[n][8];
     // 各个圆圈的类型
     int circletype[type];
     for (int i = 0; i < type; i++)
     {
-        int decimal = rand() % 256 + 1;
+        // int decimal = rand() % 256 + 1;
 
-        // 将一个8位的十进制数字转换成二进制数，用数组存储每一位,例如，定义a[8]数组，十进制128转换为a数组{1,0,0,0,0,0,0,0}。
-        decimalToBinary(decimal, binaryArray);
+        // // 将一个8位的十进制数字转换成二进制数，用数组存储每一位,例如，定义a[8]数组，十进制128转换为a数组{1,0,0,0,0,0,0,0}。
+        // decimalToBinary(decimal, binaryArray);
 
         // 圆圈的位置
         int x = rand() % n;
@@ -91,48 +91,45 @@ int main()
         {
             circleX[i] = x;
             circleY[i] = y;
-            // 限制条件,剪枝
-            {
-                if (y == 0) // 最左边不应该有指向左边的箭头
-                {
-                    binaryArray[nowcircle][5] == 0;
-                    binaryArray[nowcircle][6] == 0;
-                    binaryArray[nowcircle][7] == 0;
-                }
-                if (y == n - 1) // 最右边不应该有指向右边的箭头
-                {
-                    binaryArray[nowcircle][1] == 0;
-                    binaryArray[nowcircle][2] == 0;
-                    binaryArray[nowcircle][3] == 0;
-                }
-                if (x == n - 1) // 最上
-                {
-                    binaryArray[nowcircle][0] == 0;
-                }
-                if (x == 0) // 最下
-                {
-                    binaryArray[nowcircle][4] == 0;
-                }
-            }
+            // // 限制条件,剪枝
+            // {
+            //     if (y == 0) // 最左边不应该有指向左边的箭头
+            //     {
+            //         binaryArray[nowcircle][5] == 0;
+            //         binaryArray[nowcircle][6] == 0;
+            //         binaryArray[nowcircle][7] == 0;
+            //     }
+            //     if (y == n - 1) // 最右边不应该有指向右边的箭头
+            //     {
+            //         binaryArray[nowcircle][1] == 0;
+            //         binaryArray[nowcircle][2] == 0;
+            //         binaryArray[nowcircle][3] == 0;
+            //     }
+            //     if (x == n - 1) // 最上
+            //     {
+            //         binaryArray[nowcircle][0] == 0;
+            //     }
+            //     if (x == 0) // 最下
+            //     {
+            //         binaryArray[nowcircle][4] == 0;
+            //     }
+            // }
 
-            // // 限制条件后重新转回十进制,方便存储
-            // circletype[i] = binaryToDecimal(binaryArray);
-            // 必须存在的分支
-            // 表示是否对应成功（至少一次）
-            bool Correct_path = false;
-            for (int i1 = 0; i1 < 8; i1++)
-            {
-                // 初始节点没有前继节点
-                if (i1 == 0)
-                    break;
-                if (binaryArray[nowcircle - 1][i] == 1 && nowcircle != 0 && !Correct_path)
-                {
-                    // 对角线为1（必须有一个元素与其对应）
-                    binaryArray[nowcircle][(i + 4) % 8] == 1;
-                    // 有一个对应即可
-                    Correct_path = true;
-                }
-            }
+            // // // 限制条件后重新转回十进制,方便存储
+            // // circletype[i] = binaryToDecimal(binaryArray);
+            // // 必须存在的分支
+            // // 表示是否对应成功（至少一次）
+            // bool Correct_path = false;
+            // for (int i1 = 0; i1 < 8; i1++)
+            // {
+            //     if (binaryArray[nowcircle - 1][i1] == 1 && nowcircle != 0 && !Correct_path)
+            //     {
+            //         // 对角线为1（必须有一个元素与其对应）
+            //         binaryArray[nowcircle][(i1 + 4) % 8] == 1;
+            //         // 有一个对应即可
+            //         Correct_path = true;
+            //     }
+            // }
             nowcircle++;
         }
         else // 被占用就重置
@@ -150,10 +147,11 @@ int main()
     for (int j = 0; j < type; j++)
     {
         cout << "j:" << j << endl;
-        for (int i = 0; i < 8; i++)
-        {
-            cout << binaryArray[j][i] << " ";
-        }
+        // for (int i = 0; i < 8; i++)
+        // {
+        //     cout << binaryArray[j][i] << " ";
+        // }
+        
         cout << "\n"
              << circleX[j] << " " << circleY[j] << " ";
         cout << endl;
